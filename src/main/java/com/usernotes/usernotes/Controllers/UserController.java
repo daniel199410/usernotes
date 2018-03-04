ef3c5257aa231f4dbbc958bc427ede0d60b6eb22
Daniel Cataño <dasniel19910@gmail.com>
2018-03-03 21:28:18 -0500

    get method added

--- src/main/java/com/usernotes/usernotes/Controllers/UserController.java
+++ src/main/java/com/usernotes/usernotes/Controllers/UserController.java
@@ -1,8 +1,11 @@
 package com.usernotes.usernotes.Controllers;
 
+import com.usernotes.usernotes.Domain.Note;
+import com.usernotes.usernotes.Domain.User;
 import com.usernotes.usernotes.Repository.UserRepository;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("Users")
@@ -13,5 +16,21 @@ public class UserController {
         this.userRepository = userRepository;
     }
 
+    @GetMapping("/all")
+    public List<User> getAll(){
+        List<User> hotels = this.userRepository.findAll();
+        return hotels;
+    }
 
+    @GetMapping("/{id}")
+    public User getById(@PathVariable("id") String id){
+        User user = this.userRepository.findById(id);
+        return user;
+    }
+
+    @GetMapping("/{id}/notes")
+    public List<Note> getNotes(@PathVariable("id") String id){
+        List<Note> notes = this.userRepository.findById(id).getNotes();
+        return notes;
+    }
 }
